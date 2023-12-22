bacfc3c390595aaba445ddd1803acf19c6b453cf
vedantnamdeo <vedantnamdeo112@gmail.com>
2023-12-22 19:55:15 +0530

    deleted

--- ShowRoomManagementSystemWithList/custom_exceptions/InvalidArguementException.java
+++
@@ -1,9 +0,0 @@
-package ShowRoomManagementSystemWithList.custom_exceptions;
-
-public class InvalidArguementException extends Exception
-{
-	public InvalidArguementException(String msg)
-	{
-		super(msg);
-	}
-}
--- ShowRoomManagementSystemWithList/custom_exceptions/InvalidInputException.java
+++
@@ -1,9 +0,0 @@
-package ShowRoomManagementSystemWithList.custom_exceptions;
-
-public class InvalidInputException extends Exception
-{
-	public InvalidInputException(String msg)
-	{
-		super(msg);
-	}
-}
